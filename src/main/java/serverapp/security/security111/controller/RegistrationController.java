5636c2f955fe2373e292a5961b308809cde3a0e5
FahriYusuf <fahrir47@gmail.com>
2023-04-01 10:16:40 +0700

    Created Security

--- src/main/java/serverapp/security/security111/controller/RegistrationController.java
+++ src/main/java/serverapp/security/security111/controller/RegistrationController.java
@@ -1,6 +1,7 @@
 package serverapp.security.security111.controller;
 
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,7 +13,6 @@ import serverapp.security.security111.service.RegistrationService;
 @AllArgsConstructor
 public class RegistrationController {
     private RegistrationService registrationService;
-
     public String register(@RequestBody RegistrationRequest request){
         return registrationService.register(request);
     }
--- src/main/java/serverapp/security/security111/service/RegistrationService.java
+++ src/main/java/serverapp/security/security111/service/RegistrationService.java
@@ -1,7 +1,9 @@
 package serverapp.security.security111.service;
 
+import org.springframework.stereotype.Service;
 import serverapp.security.security111.models.RegistrationRequest;
 
+@Service
 public class RegistrationService {
     public String register(RegistrationRequest request){
         return "Works";
