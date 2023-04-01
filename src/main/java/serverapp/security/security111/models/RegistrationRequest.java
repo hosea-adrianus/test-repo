f08da5c32ff9ddc8bdbd196d9cfdcd2b4d3615e3
FahriYusuf <fahrir47@gmail.com>
2023-04-01 09:57:12 +0700

    Created Registration

---
+++ src/main/java/serverapp/security/security111/models/RegistrationRequest.java
@@ -0,0 +1,17 @@
+package serverapp.security.security111.models;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
+public class RegistrationRequest {
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
+}
---
+++ src/main/java/serverapp/security/security111/service/RegistrationService.java
@@ -0,0 +1,9 @@
+package serverapp.security.security111.service;
+
+import serverapp.security.security111.models.RegistrationRequest;
+
+public class RegistrationService {
+    public String register(RegistrationRequest request){
+        return "Works";
+    }
+}
